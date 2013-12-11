767c35e527807da53e48d5b52777b624d19ae8f7
Eugene Cheipesh <echeipesh@gmail.com>
2013-12-10 19:44:37 -0500

    Colony working and testing

--- src/main/scala/Colony.scala
+++ src/main/scala/Colony.scala
@@ -4,37 +4,70 @@
  */
 
 import akka.actor._
+import akka.actor.Props
 
+object Colony {
+  /** Start a cycle with given number of ants */
+  case class Start(ants: Int)
+  case object GiveBestTour
+  def Props(g: ActorRef):Props = akka.actor.Props(classOf[Colony], g)
+}
 /**
  * What do we do here?
  * - spawn ants to traverse the graph
  * - collect their traversals (wait for all)
  * - perform a global update
  *
+ * We can be in several states:
+ * waiting: Nothing much is going on
+ *  - Start a wave
+ *  - Give current best tour
+ *
+ * exploring: The ants, they are crawling
+ *  - buffer start request
+ *  - receive TourCompleted and update the tours
+ *  - receieve UpdateDone in response to Global update
+ *      only to best sent out after all the ants return
+ *
  * @param G Actor representing a Graph we are traversing
  */
-class Colony(G: ActorRef, antCount: Int) extends Actor {
-  var children = Set.empty[ActorRef]
-  var tours = Set.empty[List[Graph.NodeID]]
+class Colony(G: ActorRef) extends Actor {
+  import Colony._
 
-  UnleashTheAnts()
+  var children: Set[ActorRef] = Set.empty
+  //This should probably be Option[Tour]
+  var bestTour: Graph.Tour = Graph.Tour(Double.MaxValue, Nil)
 
-  def receive: Actor.Receive = {
-    case Ant.TourCompleted(path) =>
-      children -= sender
-      tours += path
-      if (children.isEmpty)
-        G ! Graph.GlobalUpdate(tours)
+  def receive = waiting
 
-    case Graph.UpdateDone  =>
-      children = Set.empty[ActorRef]
-      tours = Set.empty[List[Graph.NodeID]]
-      UnleashTheAnts()
+  val waiting: Receive = {
+    case Colony.Start(n) =>
+      children = Set.empty
+      context.become(running)
+      UnleashTheAnts(n)
+
+    case GiveBestTour =>
+      sender ! bestTour
   }
 
-  def UnleashTheAnts():Unit = {
-    for (i <- 1 to antCount) {
-      children += context.actorOf( Props(classOf[Ant], G, 0), s"Ant_$i")
+  val running: Receive = {
+    case Ant.TourCompleted(tour: Graph.Tour) =>
+      children -= sender
+      if (tour.length < bestTour.length)
+        bestTour = tour
+
+      if (children.isEmpty)
+        G ! Graph.GlobalUpdate(bestTour)
+
+    case Graph.UpdateDone  =>
+      //Once graph is consistent with final state we notify parent of the best tour we found
+      context.become(waiting)
+      context.parent ! bestTour
+  }
+
+  def UnleashTheAnts(n: Int):Unit = {
+    for (i <- 1 to n) {
+      children += context.actorOf( Ant.Props(G, 0), s"Ant_$i")
     }
   }
 }
--- src/test/scala/ColonySpec.scala
+++ src/test/scala/ColonySpec.scala
@@ -3,24 +3,39 @@
  * Date: 12/9/13
  */
 
-import akka.actor.{Props, ActorSystem}
-import akka.testkit.TestProbe
-import org.scalatest.{BeforeAndAfterAll, Matchers, FlatSpec}
+import akka.actor._
+import akka.testkit._
+import org.scalatest._
+import scala.concurrent.duration._
 
 class ColonySpec extends FlatSpec with Matchers with BeforeAndAfterAll {
   implicit val system = ActorSystem("TestSys")
   override def afterAll()  { system.shutdown() }
 
+  //We need at least 4 nodes to test TSP meaningfully
+  //We'd exepect A-D and C-B routes to be avoided
   val G_dist:Array[Array[Double]] = Array(
-    Array( 0.0, 10.0, 20.0),
-    Array(10.0,  0.0,  5.0),
-    Array(20.0,  5.0,  0.0)
+    //       A,    B,    C,    D
+    Array( 0.0,  5.0,  5.0, 20.0),
+    Array( 5.0,  0.0, 20.0,  5.0),
+    Array( 5.0, 20.0,  0.0,  5.0),
+    Array(20.0,  5.0,  5.0,  0.0)
   )
-  val G = system.actorOf( Props(classOf[Graph], G_dist), "Graph")
-  val C = system.actorOf( Props(classOf[Colony], G, 2), "Colony")
-  val probe = TestProbe()
+  val G = system.actorOf( Graph.Props(G_dist), "Graph")
 
   "Colony" should "solve TSP" in {
-    assert(false) //but does not :(
+    val probe = TestProbe()
+
+    //Colony is going to launch ants as soon as it's constructed
+    //it should report to it's parent
+    val sp = system.actorOf(
+      Props(new StepParent(Colony.Props(G), probe.ref)), "controller"
+    )
+    sp ! Colony.Start(1)
+    val tour = probe.expectMsgPF(1.second){case x: Graph.Tour => x}
+
+    tour.length should equal (20.0)
+    tour.path.toSet should equal (List(0, 1, 3, 2, 0).toSet)
+    tour.path.length should equal (5)
   }
 }
